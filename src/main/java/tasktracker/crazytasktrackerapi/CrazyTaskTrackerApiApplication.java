bc02e650624a99e2de771cf10268e8a6835a4cfa
Zhenya <zsydun@gamil.com>
2024-09-25 19:14:32 +0300

    first commit

---
+++ src/main/java/tasktracker/crazytasktrackerapi/CrazyTaskTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package tasktracker.crazytasktrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazyTaskTrackerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrazyTaskTrackerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tasktracker/crazytasktrackerapi/CrazyTaskTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tasktracker.crazytasktrackerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrazyTaskTrackerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
